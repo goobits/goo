4d65a8300ffeceee33c2cc6b15d29417cfe3bbbd
Miko Meow <101564+mudcube@users.noreply.github.com>
2026-06-04 00:03:59 -0700

    Move toolbar styles into Goo

--- src/toolbar/index.ts
+++ src/toolbar/index.ts
@@ -1,5 +1,7 @@
 /// <reference path="../svelte.d.ts" />
 
+import './Toolbar.css'
+
 export type { CreateFloatingToolbarViewOptions, FloatingToolbarElement } from './createFloatingToolbarView.ts'
 export { createFloatingToolbarView } from './createFloatingToolbarView.ts'
 export type { CreateToolbarToolButtonOptions, ToolbarToolButtonElement } from './createToolbarToolButton.ts'
